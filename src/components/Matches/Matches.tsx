efb2dd2f9b3970cb3b4d660bdf497d3f5089bbc8
Nicole Forseth <forseth.nico@gmail.com>
2022-10-30 17:45:10 -0600

    Create swiper slides for matches

--- src/components/Matches/Matches.tsx
+++ src/components/Matches/Matches.tsx
@@ -1,50 +1,73 @@
-import React, {Component} from "react";
-import { Link } from "react-router-dom";
+import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { dogData } from "../../utilities/interfaces";
 import { Navigation, Mousewheel, Keyboard, Pagination } from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
-import "./Matches.css"
-
+import "./Matches.css";
+import info from "../../assets/info.png";
+import animal from "../../assets/info.png";
 
 type MatchDog = {
-    id: number,
-    name: string, 
-    image: string, 
-    favorite: boolean
-}
+  id: number;
+  name: string;
+  image: string;
+  favorite: boolean;
+};
 
-const Matches = ({filteredDogs, onFavorite}: {filteredDogs: MatchDog[], onFavorite: (id:number) => void }) => {
+const Matches = ({
+  filteredDogs,
+  onFavorite,
+}: {
+  filteredDogs: dogData[];
+  onFavorite: (id: number) => void;
+}) => {
+  const handleClick = (event: any) => {
+    // onFavorite(event.target.id);
+    console.log(event.target.id);
+  };
 
-    const handleClick = (event:any) => {
-        onFavorite(event.target.value);
-    }
+  const createSwiperSlides = () => {
+    let matchedDogs = filteredDogs.map((dog) => {
+      return (
+        <SwiperSlide className="swiper-slide" key={dog.id}>
+          <img src={dog.image.url} alt="dog" className="dog-image" />
+          <h2 className="dog-name">{dog.name}</h2>
+          <div className="icons">
+            <img src={info} alt="information" className="info-image" />
+            <img
+              src={animal}
+              alt="favorite"
+              id={dog.id}
+              className="favorite-image"
+              onClick={handleClick}
+            />
+          </div>
+        </SwiperSlide>
+      );
+    });
+    return matchedDogs;
+  };
+  return (
+    <div className="matches-container">
+      <h1 className="matches-header">Your Matches:</h1>
+      <section className="matches-swiper">
+        <Swiper
+          modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+          slidesPerView={1}
+          pagination={{ clickable: true }}
+          navigation={true}
+          keyboard={true}
+          mousewheel={true}
+          className="all-swiper-matches"
+        >
+          {createSwiperSlides()}
+        </Swiper>
+      </section>
+    </div>
+  );
+};
 
-const createSwiperSlides = () => {
-    let matchedDogs = filteredDogs.map(dog => {
-        return (
-            <SwiperSlide className= "swiper-slide" key={dog.id}>
-                <div className ="match-page">
-                    <img src = {dog.image.url} alt = "dog" className="dog-image"/>
-                    <h2 className = "dog-name">{dog.name}</h2>
-                    <img src = {}
-                </div>
-            </SwiperSlide>
-        )
-    })
-}
-
-
-
-    return (
-        <div className = "matches-container">
-            <h1 className="matches-header">Your Matches:</h1>
-            
-
-        </div>
-    )
-}
-
-export default Matches
+export default Matches;
