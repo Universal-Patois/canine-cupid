967b23d55b9d7720f6c2afa12c8c5c4dc357f4d4
Nicole Forseth <forseth.nico@gmail.com>
2022-10-30 20:52:28 -0600

    Remove react strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import App from './Components/App/App';
+import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
